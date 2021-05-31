6752e80e1833f65320a610c6aac2c8e4397d9b56
Ben Dyer <43922444+ben-dyer@users.noreply.github.com>
2021-05-31 12:59:00 +0900

    Fix file path and add deps

--- src/node.js
+++ src/node.js
@@ -1,36 +1,27 @@
-const visit = require('unist-util-visit');
+const visit = require("unist-util-visit")
 
-const getPageDir = ({ parent }) => parent.split(" ").shift().split("/").slice(0, -1).join("/");
-const escape = txt => txt.replace(/"/g, '&quot;');
+const escape = txt => txt.replace(/"/g, "&quot;")
 
-const MARKDOWN_TAG = "vega";
+const MARKDOWN_TAG = "vega"
 
-module.exports = (
-  {
-    markdownNode,
-    markdownAST,
-  },
-) => {
+module.exports = ({ markdownNode, markdownAST }) => {
   visit(markdownAST, `inlineCode`, node => {
-    const { value } = node;
+    const { value } = node
 
     if (value.startsWith(`${MARKDOWN_TAG}:`)) {
-      const dir = getPageDir(markdownNode);
-      const file = value.split(':').pop();
-      const path = `${dir}/${file}`;
-
-      console.log('require fs!');
-      const fs = require(`fs`);
+      const file = value.split(":").pop()
+      const path = markdownNode.fileAbsolutePath.replace(/(?<=\/)[^\/]+$/, file)
+      const fs = require(`fs`)
       if (!fs.existsSync(path)) {
-        throw Error(`Invalid file specified; no such file "${path}"`);
+        throw Error(`Invalid file specified; no such file "${path}"`)
       }
 
-      const spec = fs.readFileSync(path, `utf8`);
+      const spec = fs.readFileSync(path, `utf8`)
 
-      node.type = `html`;
-      node.value = `<vega spec="${escape(spec)}" />`;
+      node.type = `html`
+      node.value = `<vega spec="${escape(spec)}" />`
     }
   })
 
-  return markdownAST;
-};
+  return markdownAST
+}
